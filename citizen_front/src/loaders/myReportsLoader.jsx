5d560382607a309b3b9530a6a7d681d3d4ee2d92
udaydhikale07 <meaditya1aditya@gmail.com>
2026-09-06 13:48:48 +0530

    the frontend updated a little

--- citizen_front/src/loaders/myReportsLoader.jsx
+++ citizen_front/src/loaders/myReportsLoader.jsx
@@ -6,23 +6,11 @@ export async function myReportsLoader() {
   try {
     const url = `${API_URL}/api/reports/my`;
 
-    // console.log("=================================");
-    // console.log("myReportsLoader EXECUTED");
-    // console.log("API_URL:", API_URL);
-    // console.log("Request URL:", url);
-    // console.log("=================================");
-
     const response = await fetchWithAuth(url, {
       method: "GET",
     });
 
-    // console.log("Response status:", response.status);
-    // console.log("Response URL:", response.url);
-    // console.log(
-    //   "Response content type:",
-    //   response.headers.get("content-type")
-    // );
-
+ 
     const data = await response.json();
 
     console.log("myReportsLoader data:", data);
